76db8f9992990d96530e8e157b29ebb9d2c91f1c
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-04-04 20:33:41 +0200

    Add ErrorToast component for error handling in chat

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import ChatHeader from './components/ChatHeader'
 import ChatMessages from './components/ChatMessages'
 import ChatInput from './components/ChatInput'
 import useChat from './hooks/useChat'
+import ErrorToast from './components/ErrorToast'
 
 export default function App() {
   const {messages, loading, error, sendMessage} = useChat()
 
   return (
-    <div id="chat-layout">
-      <ChatHeader />
-      <ChatMessages messages={messages} />
-      <ChatInput onSend={sendMessage} />
-    </div>
+    <>
+      <div id="chat-layout">
+        <ChatHeader />
+        <ChatMessages messages={messages} />
+        <ChatInput onSend={sendMessage} disabled={loading} />
+      </div>
+      {error && <ErrorToast error={error} />}
+    </>
   )
 }
