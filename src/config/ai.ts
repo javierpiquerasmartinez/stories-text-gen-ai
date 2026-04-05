5126ba81fbd89b013b05acf2d85534e26522599d
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-04-05 12:30:29 +0200

    Refactor provider configuration to switch from GROQ to OpenAI; update model and API key

--- src/config/ai.ts
+++ src/config/ai.ts
@@ -8,10 +8,10 @@ export const providerConfig: {
     stream: boolean,
     apiKey: string
     } = {
-    provider: 'groq',
-    baseURL: 'https://api.groq.com/openai/v1',
-    model: 'llama-3.1-8b-instant',
+    provider: 'openai',
+    // baseURL: 'https://api.groq.com/openai/v1',
+    model: 'gpt-4o-mini',
     temperature: 0.7,
     stream: true,
-    apiKey: import.meta.env.VITE_GROQ_API_KEY
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY
 }
