dcf63fb176fac080716b0c71f6f2d4c03c488bd0
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-04-06 19:54:25 +0200

    Remove unused import of ChatHeader and SidebarContext from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css'
-import ChatHeader from './components/ChatHeader'
 import { Outlet } from 'react-router-dom'
-import { useSidebar } from './context/SidebarContext'
 import BottomNav from './components/BottomNav'
 
 export default function App() {
