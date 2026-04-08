83bd33e62c1b8696d19fc03d1180e4f32a0c45b8
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-04-08 17:38:12 +0200

    Error Toast onclose functionality

--- src/components/ErrorToast.tsx
+++ src/components/ErrorToast.tsx
@@ -1,7 +1,24 @@
-export default function ErrorToast({ error }: { error: string }) {
+import { useEffect } from "react";
+
+export default function ErrorToast({ error, closeTimeout, onClose }: { error: string; closeTimeout?: number, onClose?: () => void }) {
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+    if (closeTimeout && onClose) {
+      timer = setTimeout(() => {
+        onClose();
+      }, closeTimeout)
+    }
+    return () => {
+      if (timer)
+        clearTimeout(timer);
+    }
+  }, [error, closeTimeout, onClose])
+
   return (
     <div id="error-toast">
       <p>{error}</p>
+      <button onClick={onClose} className="error-toast__close" aria-label="Dismiss error">&#x00D7;</button>
     </div>
   )
 }
